3d7bd178b8366f3fff23e0f7801b6f76742a629e
ishitad05 <ishitadhuri@gmail.com>
2024-10-20 12:14:45 +0530

    first commit

---
+++ conversion python.py
@@ -0,0 +1,152 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+12
+<class 'int'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 4, in <module>
+    b=list(a)
+TypeError: 'int' object is not iterable
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+12
+<class 'int'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 4, in <module>
+    b=list(a)
+TypeError: 'int' object is not iterable
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+12
+<class 'int'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 4, in <module>
+    b=list(a)
+TypeError: 'int' object is not iterable
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+12
+<class 'int'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 11, in <module>
+    b=tuple(a)
+TypeError: 'int' object is not iterable
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+2.5
+<class 'float'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 18, in <module>
+    b=list(a)
+TypeError: 'float' object is not iterable
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+2.5
+<class 'float'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 25, in <module>
+    b=tuple(a)
+TypeError: 'float' object is not iterable
+>>> 
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 32, in <module>
+    b=list(a)
+TypeError: 'bool' object is not iterable
+>>> 
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 32, in <module>
+    b=list(a)
+TypeError: 'bool' object is not iterable
+>>> 
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 40, in <module>
+    b=tuple(a)
+TypeError: 'bool' object is not iterable
+>>> 
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+(20+10j)
+<class 'complex'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 48, in <module>
+    b=list(a)
+TypeError: 'complex' object is not iterable
+>>> 
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+(20+10j)
+<class 'complex'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 55, in <module>
+    b=tuple(a)
+TypeError: 'complex' object is not iterable
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+1234
+<class 'str'>
+['1', '2', '3', '4']
+<class 'list'>
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+1234
+<class 'str'>
+('1', '2', '3', '4')
+<class 'tuple'>
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+[1234]
+<class 'list'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 76, in <module>
+    b=int(a)
+TypeError: int() argument must be a string, a bytes-like object or a real number, not 'list'
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+[1234]
+<class 'list'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 83, in <module>
+    b=float(a)
+TypeError: float() argument must be a string or a real number, not 'list'
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+[1234]
+<class 'list'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 90, in <module>
+    b=complex(a)
+TypeError: complex() first argument must be a string or a number, not 'list'
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+[12]
+<class 'list'>
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py", line 90, in <module>
+    b=complex(a)
+TypeError: complex() first argument must be a string or a number, not 'list'
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+[1234]
+<class 'list'>
+[1234]
+<class 'list'>
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+[1234]
+<class 'list'>
+[1234]
+<class 'str'>
+
+===== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/conversion.py ====
+[1234]
+<class 'list'>
+(1234,)
+<class 'tuple'>
